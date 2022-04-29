108cd278cddd7117963d3f3dc8a53f04795a1dc3
pablovalenzuela <pjvalenzuela@gmail.com>
2022-04-28 23:17:30 -0300

    proyecto e commerce

---
+++ js/clases.js
@@ -0,0 +1,66 @@
+//variables
+const clases = [
+
+    {
+        id:1,
+        name : "Pilates",
+        img: 'img/todas1.jpg'
+    },
+    {
+        id:2,
+        name : "Yoga",
+        img: 'img/todas2.jpg'
+    },
+    {
+        id:3,
+        name : "Masajes",
+        img: 'img/todas3.jpg'
+    },
+    {
+        id:4,
+        name : "Reiki Usui",
+        img: 'img/todods2.jpg'
+    },
+
+
+
+];
+
+const contenedorClases = document.querySelector(".contenedor-clases");
+const listadoFavoritos = document.querySelector('.listado-favoritos');
+
+//eventos
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarClases();
+})
+
+//funciones
+function mostrarClases() {
+    clases.forEach(clase => {
+        //scripting
+        const divClase = document.createElement('div');
+        divClase.classList.add('card');
+
+        const imgClase = document.createElement('img');
+        imgClase.src = clase.img;
+        imgClase.classList.add('imagen-clase');
+
+        const tituloClase = document.createElement('h3')
+        tituloClase.textContent = clase.name;
+
+        const btnFavorito = document.createElement('button');
+        btnFavorito.className = "btn-favorito"
+        btnFavorito.textContent = "Agregar al carrito";
+       btnFavorito.addEventListener('click', respuestaClick)
+
+       function respuestaClick(){
+           alert('se agrego ' + clase.name + ' al carrito')
+       }
+
+        divClase.appendChild(imgClase);
+        divClase.appendChild(tituloClase);
+        divClase.appendChild(btnFavorito);
+
+        contenedorClases.appendChild(divClase);
+    })
+} 
